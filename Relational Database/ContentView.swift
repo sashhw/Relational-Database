db5317aaf8cb5ea281b62c3b92c504be9d56db44
sashhw <sash.walk@gmail.com>
2021-09-03 12:19:59 -0500

    Initial Commit

---
+++ Relational Database/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Relational Database
+//
+//  Created by Sasha on 9/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Relational Database/Relational_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Relational_DatabaseApp.swift
+//  Relational Database
+//
+//  Created by Sasha on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Relational_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
